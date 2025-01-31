6bdc5e8cd08cfac94caa46ea7c5526647cce50c5
Vlad Zbaradskiy <vladzbaradskij24@gmail.com>
2025-02-01 00:44:51 +0300

    fix3

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,15 @@
-export const normalTag = (tag) => {
+export const normalTag = (tag: any) => {
   if (typeof tag !== 'string') return ''
   return tag.length > 10 ? tag.slice(0, 10).replace(/\n/g, '') + '...' : tag
 }
 
-export const normalTitle = (title) => {
+export const normalTitle = (title: any) => {
   if (!/\w/.test(title)) return 'untitled'
   if (title.length > 40) return title.slice(0, 40) + '...'
   return title
 }
 
-export const normalDesc = (description) => {
+export const normalDesc = (description: any) => {
   if (!/\w/.test(description)) return 'no descpription...'
   if (description.length > 250) return description.slice(0, 250) + '...'
   return description
